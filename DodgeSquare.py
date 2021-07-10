53e9a57389e80e820ba28b3b245a18ab28d1ab6a
lucastoll <lucasfernandestolotto@gmail.com>
2021-07-09 21:09:48 -0300

    Jogo finalizado
    Tela inicial adicionada
    Imagens pos jogo
    melhorias gerais

--- DodgeSquare.py
+++ DodgeSquare.py
@@ -50,7 +50,7 @@ timer=0 #Contador de tempo do jogo
 # Sons
 pygame.mixer.music.set_volume(0.2)
 pygame.mixer.music.load('content/xDeviruchi - Minigame .mp3') #Background music
-pygame.mixer.music.play(-1)#musica principal play forever
+
 
 damage_sound = pygame.mixer.Sound('content/smas_damage.wav')
 heartup_sound = pygame.mixer.Sound('content/smas_heart.wav')
@@ -61,6 +61,17 @@ win_sound = pygame.mixer.Sound('content/smw_win.wav')
 #Imagens
 heart = pygame.image.load('content/heart.png')
 heart = pygame.transform.scale(heart, (50, 50))
+
+ganhou = pygame.image.load('content/ganhou.jpeg')
+perdeu = pygame.image.load('content/perdeu.jpeg')
+perdeu2 = pygame.image.load('content/perdeu2.jpeg')
+perdeu2 = pygame.transform.scale(perdeu2, (500, 500))
+recorde = pygame.image.load('content/recorde.jpeg')
+recorde = pygame.transform.scale(recorde, (400, 400))
+wrecord = pygame.image.load('content/wrecord.png')
+setas = pygame.image.load('content/setas.jpg')
+setas = pygame.transform.scale(setas, (200, 100))
+
 
 #Controle linhas horizontais
 xlinha = -1
@@ -76,6 +87,60 @@ yrec = 384
 xpontos = 492 #Um pouco acima do player para ele entender como funciona
 ypontos = 184
 
+start = True
+while start:
+    for event in pygame.event.get():  # Loop verficação de ação, para fechar o programa quando pedido.
+        if event.type == QUIT:
+            pygame.quit()
+            exit()
+        if event.type == KEYDOWN:  # Movimentação quadrado com setas
+            if event.key == K_r:
+                start = False
+    pygame.draw.line(tela, (0, 255, 0), [0, 50], [1024, 50], 5)
+    pygame.draw.rect(tela, (0, 255, 0), (950, 0, 5, altura))
+    pygame.draw.rect(tela, (0, 0, 255), (850, 100, 40, 40))
+    pygame.draw.rect(tela, (250, 253, 15), (800, 150, 10, 10))
+    fonte_start = pygame.font.SysFont('Bahnschrift', 60, True)  # Inicializar fonte
+    fonte_start2 = pygame.font.SysFont('Arial', 20, True)  # Inicializar fonte
+    mensagem_start = 'DodgeSquare'
+    mensagem_start2 = 'Como jogar:'
+    mensagem_start3 = 'W'
+    mensagem_start4 = 'ASD'
+    mensagem_start5 = 'Move o quadrado'
+    mensagem_start6 = 'Você controla o quadrado azul e precisa pegar o quadrado amarelo enquanto desvia das linhas'
+    mensagem_start7 = 'Teleporta o quadrado'
+    mensagem_start8 = 'Se o quadrado azul ultrapassar algum limite da tela ele aparece no limite contrário'
+    mensagem_start9 = 'Você tem 3 vidas e precisa de 30 pontos para ganhar.'
+    mensagem_start10 = 'Pressione R para começar'
+
+    texto_start = fonte_start.render(mensagem_start, True, (255, 255, 255))
+    texto_start2 = fonte_start2.render(mensagem_start6, True, (255, 255, 255))
+    texto_start3 = fonte_start2.render(mensagem_start2, True, (255, 255, 255))
+    texto_start4 = fonte_start.render(mensagem_start3, True, (255, 255, 255))
+    texto_start5 = fonte_start.render(mensagem_start4, True, (255, 255, 255))
+    texto_start6 = fonte_start2.render(mensagem_start5, True, (255, 255, 255))
+    texto_start7 = fonte_start2.render(mensagem_start7, True, (255, 255, 255))
+    texto_start8 = fonte_start2.render(mensagem_start8, True, (255, 255, 255))
+    texto_start9 = fonte_start2.render(mensagem_start9, True, (255, 255, 255))
+    texto_start10 = fonte_start2.render(mensagem_start10, True, (255, 255, 255))
+
+    tela.blit(texto_start, (largura / 3.3, 100))
+    tela.blit(texto_start2, (30, 200))
+    tela.blit(texto_start3, (largura / 2 - 80, 240))
+    tela.blit(texto_start4, (300, 300))
+    tela.blit(texto_start5, (260, 350))
+    tela.blit(texto_start6, (240, 420))
+    tela.blit(setas, (600, 320))
+    tela.blit(texto_start7, (600, 420))
+    tela.blit(texto_start8, (100, 700))
+    tela.blit(heart, (430, 500))
+    tela.blit(heart, (480, 500))
+    tela.blit(heart, (530, 500))
+    tela.blit(texto_start9, (230, 550))
+    tela.blit(texto_start10, (370, 600))
+
+    pygame.display.update()  # Atualiza o display com os textos novos
+pygame.mixer.music.play(-1)  # musica principal play forever
 while True:
     if vidas == 3:
         tela.blit(heart, (0,0))
@@ -117,6 +182,7 @@ while True:
         mensagem_win  = 'Você ganhou'
         mensagem_win2 = 'Recompensa = +2 QI'
         mensagem_win3 = 'Pressione R para jogar novamente'
+        mensagem_record = 'RECORDE MUNDIAL!!!!!!!!!!!!!'
 
         texto_formatado = fonte.render(mensagem, True, (255, 255, 255))  # Renderizar o texto
         tela.blit(texto_formatado, (largura / 2 - 100, 1))
@@ -125,15 +191,30 @@ while True:
         texto_win = fonte.render(mensagem_win, True, (255, 255, 255))
         texto_win2 = fonte.render(mensagem_win2, True, (255, 255, 255))
         texto_win3 = fonte.render(mensagem_win3, True, (255, 255, 255))
-        if pontos >= 30:
+        texto_recorde = fonte.render(mensagem_record, True, (255, 255, 255))
+        if (pontos >= 30) & (pontos < 50):
             win_sound.play()
             tela.blit(texto_win, (largura/2 - 130, 100))
             tela.blit(texto_win2, (largura/2 - 200, 200))
             tela.blit(texto_win3, (largura/2 - 320, 300))
+            tela.blit(ganhou, ((largura/2 - 350, 400)))
+        elif pontos >= 50:
+            win_sound.play()
+            tela.blit(texto_recorde, (largura / 2 - 250, 100))
+            tela.blit(texto_win2, (largura / 2 - 200, 200))
+            tela.blit(texto_win3, (largura / 2 - 320, 300))
+            tela.blit(wrecord, ((largura / 2 - 350, 400)))
+            tela.blit(recorde, ((largura / 2, 350)))
         else:
             gameover_sound.play()
             tela.blit(texto_loss, (largura / 2 - 120, 100))
             tela.blit(texto_loss2, (largura / 2 - 320, 200))
+            perdeu_imagem = randint(1, 2)
+            if perdeu_imagem == 1:
+                tela.blit(perdeu, (largura / 2 - 300, 280))
+            else:
+                tela.blit(perdeu2, (largura / 2 - 240, 300))
+
         dead = True
         while dead: #while dead == True
             for event in pygame.event.get():  # Loop verficação de ação, para fechar o programa quando pedido.
@@ -209,8 +290,8 @@ while True:
     #Colisão player x ponto
     if player.colliderect(ponto):
         pontos += 1
-        xpontos = randint(24, 924)
-        ypontos = randint(100, 668)
+        xpontos = randint(104, 924)
+        ypontos = randint(100, 690)
         coin_sound.play()
     # Movimentação quadrado com wasd pressionando
     if pygame.key.get_pressed()[K_a]:
@@ -231,7 +312,8 @@ while True:
     if yrec >= altura:  # 768
         yrec = 0
 
+
     # printar texto
-    mensagem2 = f'VE {velocidade} TI {timer}' #Pontuação mensagem
-    texto_formatado2 = fonte.render(mensagem2, True, (255, 255, 255)) #Renderizar o texto
-    tela.blit(texto_formatado2, (largura - 500, 100))
+    # mensagem2 = f'VE {velocidade} TI {timer}' #Pontuação mensagem
+    # texto_formatado2 = fonte.render(mensagem2, True, (255, 255, 255)) #Renderizar o texto
+    # tela.blit(texto_formatado2, (largura - 500, 100))
